cbab385cadb498804727c9f85821185c45821caa
Miikka Koskinen <miikka.koskinen@iki.fi>
2016-07-11 21:21:35 +0300

    Clarify source

--- test/clj/vega_tools/testing.clj
+++ test/clj/vega_tools/testing.clj
@@ -2,6 +2,8 @@
   "Utilities for writing unit tests."
   (:require [cljs.test]))
 
+;; Derived from
+;; <https://github.com/clojure/clojurescript/blob/d06a4f4273a93bd73876867c93e3018069f5836f/src/main/cljs/cljs/test.clj#L113>
 (defmethod cljs.test/assert-expr 'thrown-with-ex-data? [menv msg form]
   (let [data (second form)
         body (nthnext form 2)]
@@ -11,7 +13,7 @@
        (catch :default e#
          (if (= (ex-data e#) ~data)
            (cljs.test/do-report
-            {:type :pass, :message ~msg, :expected '~form,:actual e#})
+            {:type :pass, :message ~msg, :expected '~form, :actual e#})
            (cljs.test/do-report
-            {:type :fail, :message ~msg, :expected '~form,:actual e#}))
+            {:type :fail, :message ~msg, :expected '~form, :actual e#}))
          e#))))
